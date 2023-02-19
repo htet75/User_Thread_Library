558e2642629992c607a0589ddbaf27c99ed2f85f
htet75 <hmyat@ucdavis.edu>
2023-02-19 02:27:11 -0800

    Implemented preempt

--- libuthread/preempt.c
+++ libuthread/preempt.c
@@ -15,23 +15,62 @@
  */
 #define HZ 100
 
+/*Global variables to keep track of previous signal action and timer configuration*/
+struct sigaction previous_action;
+struct itimerval previous_timer;
+
+/*Signal Handler for SIGVTALARM*/
+void timer_interrupt_handler(int signal)
+{
+	uthread_yield();	//force the current thread to yield
+}
+
 void preempt_disable(void)
 {
-	/* TODO Phase 4 */
+	sigset_t signal;
+	/*Initialize the signal mask*/
+    sigemptyset(&signal);
+    sigaddset(&signal, SIGVTALRM);
+	/*block SIGVTALRM */
+    sigprocmask(SIG_BLOCK, &signal, NULL);
 }
 
 void preempt_enable(void)
 {
-	/* TODO Phase 4 */
+	sigset_t alarm;
+	/*Initialize the signal mask*/
+    sigemptyset(&slgnal);
+    sigaddset(&signal, SIGVTALRM);
+	/*unblock SIGNTALRM*/
+    sigprocmask(SIG_UNBLOCK, &signal, NULL);
 }
 
 void preempt_start(bool preempt)
 {
 	/* TODO Phase 4 */
+	struct signal action;
+	struct itimerval timer; 
+
+	/* Signal handler for SIGVTALRM */
+	action.sa_handler = timer_interrupt_handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = 0;
+    sigaction(SIGVTALRM, &action, &previous_action);
+
+	/* Timer configuration to fire an alarm a hundred times per second */
+	timer.it_value.tv_sec = 0;
+    timer.it_value.tv_usec = 1000000 / HZ;
+    timer.it_interval.tv_sec = 0;
+    timer.it_interval.tv_usec = 1000000 / HZ;
+    setitimer(ITIMER_VIRTUAL, &timer, &previous_timer);
 }
 
 void preempt_stop(void)
 {
-	/* TODO Phase 4 */
+	/*Restore the previous action for SIGVTALRM*/
+	sigaction(SIGVTALRM, &previous_action, NULL);
+	
+	/*Restore the previous timer configuration for SIGVTALRM*/
+    setitimer(ITIMER_VIRTUAL, &previous_timer, NULL);
 }
 
