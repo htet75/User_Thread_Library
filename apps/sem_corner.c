f3ecadcd1ed7a877777fd04a1571b039af075471
htet75 <hmyat@ucdavis.edu>
2023-02-20 14:19:57 -0800

    semaphore corner case test

---
+++ apps/sem_corner.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sem.h"
+#include <uthread.h>
+
+sem_t sem;
+
+void threadC() {
+    sem_down(sem);
+    printf("Thread C got the semaphore!\n");
+
+    /* Simulate some work that requires the semaphore */
+    for (int i = 0; i < 5; i++) {
+        printf("Thread C is doing some work...\n");
+    }
+
+    printf("Thread C is releasing the semaphore\n");
+    sem_up(sem);
+}
+
+void threadB() {
+    uthread_create(threadC, NULL);
+    sem_up(sem);
+    printf("Thread B released the semaphore\n");
+}
+
+void threadA() {
+    uthread_create(threadB, NULL);
+    sem_down(sem);
+    printf("Thread A got the semaphore!\n");
+
+    /* Simulate some work that requires the semaphore */
+    for (int i = 0; i < 5; i++) {
+        printf("Thread A is doing some work...\n");
+    }
+
+    printf("Thread A is releasing the semaphore\n");
+    sem_up(sem);
+}
+
+int main() {
+    sem = sem_create(0);
+
+    uthread_run(false, threadA, NULL);
+
+    sem_destroy(sem);
+    return 0;
+}
