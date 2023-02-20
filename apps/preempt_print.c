5cf8f50cc2d676a88fdd004da498687afea47ebd
bduynguy <bduynguy@COE-CS-pc33.OU.AD3.UCDAVIS.EDU>
2023-02-19 16:08:13 -0800

    Updated test case checking for preemption

--- apps/preempt_print.c
+++ apps/preempt_print.c
@@ -11,32 +11,59 @@
 #include <private.h>
 #include <uthread.h>
 
-void thread3(void* arg)
+void thread3()
 {
-    (void)arg;
-    while (true) {
-        printf("Thread 3\n");
-        sleep(1);
+    time_t start, end;
+    time(&start);
+    int count = 0;
+    while (count < 10) {
+        time(&end);
+        if (difftime(end, start) >= 1.0) 
+        {
+            printf("Thread 3\n");
+            time(&start);
+            count++;
+        }
+        // uthread_yield();
+        // sleep(1);
     }
 }
 
-void thread2(void* arg)
+void thread2()
 {
-    (void)arg;
+    time_t start, end;
     uthread_create(thread3, NULL);
-    while (true) {
-        printf("Thread 2\n");
-        sleep(1);
+    time(&start);
+    int count = 0;
+    while (count < 10) {
+        time(&end);
+        if (difftime(end, start) >= 1.0) 
+        {
+            printf("Thread 2\n");
+            time(&start);
+            count++;
+        }
+        // uthread_yield();
+        // sleep(1);
     }
 }
 
-void thread1(void* arg)
+void thread1()
 {
-    (void)arg;
+    time_t start, end;
     uthread_create(thread2, NULL);
-    while (true) {
-        printf("Thread 1\n");
-        sleep(1);
+    time(&start);
+    int count = 0;
+    while (count < 10) {
+        time(&end);
+        if (difftime(end, start) >= 1.0) 
+        {
+            printf("Thread 1\n");
+            time(&start);
+            count++;
+        }
+        // uthread_yield();
+        // sleep(1);
     }
 }
 
